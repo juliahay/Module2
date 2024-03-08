5435e07d18cd1e85f188c114411b4f8e33aa712e
Julia Hay <jfh2152@columbia.edu>
2024-03-07 19:04:38 -0500

    Updated ESP32 code for one potentiameter set up.

--- sketch_feb29a/sketch_feb29a.ino
+++ sketch_feb29a/sketch_feb29a.ino
@@ -6,7 +6,7 @@
 
 TFT_eSPI tft = TFT_eSPI();
 
-int xyzPins[] = {13, 12, 14}; //x,y,z pins
+int xyzPins[] = {2, 15, 13, 12}; //x,y,z pins
 void setup() {
   tft.init();
   tft.setRotation(1);
@@ -14,8 +14,6 @@ void setup() {
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE);
   Serial.begin(115200);
-  //pinMode(xyzPins[2], INPUT_PULLUP); //z axis is a button.
-  pinMode(12, INPUT_PULLUP);
   
 }
 
@@ -25,58 +23,74 @@ int bGoal = random(0, 256);
 
 int reset = 1;
 int submit = 1;
-int hint = 1;
 
-int rUser = 0;
-int gUser = 0;
-int bUser = 0;
-
-int bVal = 1;
+int switchMode = 1;
+int mode = 0;
 
 void loop() {
 
   JsonDocument sensorVals;
 
-  //GOAL R, G, B values
-  //if reset == 0, change rGoal, gGoal, bGoal values
-  //if reset == 0, reset rUser, gUser, bUser
+  //reset game
+  pinMode(xyzPins[0], INPUT_PULLUP); 
+  reset = digitalRead(xyzPins[0]); //2
+  if (reset == 0) {
+    //if reset == 0, change rGoal, gGoal, bGoal values
+    rGoal = random(0, 256);
+    gGoal = random(0, 256);
+    bGoal = random(0, 256);
+    tft.fillScreen(TFT_BLACK);
+  }
+
+  //switchMode value (0 = change mode, 1 = stay mode)
+  //changing mode
+  pinMode(xyzPins[1], INPUT_PULLUP); 
+  switchMode = digitalRead(xyzPins[1]); //15
+  sensorVals["switch"] = switchMode;
+  if (switchMode == 0) {
+    if (mode == 0) {
+      mode = 1;
+    } else if (mode == 1) {
+      mode = 2;
+    } else if (mode == 2) {
+      mode = 0;
+    }
+    tft.fillScreen(TFT_BLACK);
+  }
+
+  //Printing and sending GOAL R, G, B values
   sensorVals["rG"] = rGoal;
   sensorVals["gG"] = gGoal;
   sensorVals["bG"] = bGoal;
   tft.drawString("(" + String(rGoal) + ", " + String(gGoal) + ", " + String(bGoal) + ")", 30, 50, 4);
 
+  
+  //mode: 0 == red, 1 == green, 2 == blue
+  //changing color controlled
+  sensorVals["mode"] = mode;
+  if (mode == 0) {
+      tft.drawString("R", 100, 90, 4);
+  } else if (mode == 1) {
+      tft.drawString("G", 100, 90, 4);
+  } else if (mode == 2) {
+      tft.drawString("B", 100, 90, 4);
+  }
 
   //Submit value (0 == submit, 1 == resting state)
-  submit = digitalRead(12);
+  pinMode(xyzPins[2], INPUT_PULLUP); 
+  submit = digitalRead(xyzPins[2]); //13
   sensorVals["s"] = submit;
 
-  //Hint Value (0 == get hint, 1 == resting state)
-  //sensorVals["h"] = hint;
-
-  //Changing R value
-  int pVal = analogRead(13);
+  //Changing color value
+  pinMode(xyzPins[3], INPUT_PULLUP);
+  int pVal = analogRead(xyzPins[3]); //12
   int v = pVal / 16;
   if (v < 1) {
     v = 0;
   } else if (v > 255) {
     v = 255;
   }
-  rUser = v;
-  sensorVals["r"] = rUser;
-
-  //Changing G value
-  pVal = analogRead(32);
-  v = pVal / 16;
-  if (v < 1) {
-    v = 0;
-  } else if (v > 255) {
-    v = 255;
-  }
-  gUser = v;
-  sensorVals["g"] = gUser;
-  
-  //Changing B value
-  sensorVals["b"] = bUser;
+  sensorVals["colorVal"] = v;
   
   
   //sensorVals["b"] = bVal;
